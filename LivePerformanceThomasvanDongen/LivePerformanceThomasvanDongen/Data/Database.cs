5f29418075ca3cdf6583f0ec29ffb505e678b2a1
Dongen, Thomas T.G.T.A. van <tgta.vandongen@student.fontys.nl>
2016-06-22 13:32:29 +0200

    Database methodes gemaakt.

--- LivePerformanceThomasvanDongen/LivePerformanceThomasvanDongen/Data/Database.cs
+++ LivePerformanceThomasvanDongen/LivePerformanceThomasvanDongen/Data/Database.cs
@@ -1,13 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
 
 namespace LivePerformanceThomasvanDongen.Data
 {
     public class Database
     {
-
+        private const string USER = "dbi323229";
+        /// <summary>
+        /// The password used to connect with the database.
+        /// </summary>
+        private const string PASSWORD = "wXkDxOdQUV";
+        /// <summary>
+        /// The oracle-specific string used to connect with the database.
+        /// </summary>
+        private const string CONNECTION_STRING = "User Id= " + USER + ";Password= " + PASSWORD + ";Data Source=" + @" //192.168.15.50:1521/fhictora" + ";";
+        /// <summary>
+        /// connection aangemaakt zodat deze gebruikt kan worden.
+        /// </summary>
+        private readonly OracleConnection Conn;
+        public bool OpenConnection()
+        {
+            Console.WriteLine(Conn.State == System.Data.ConnectionState.Open
+                ? "Database Connection was already open"
+                : "Opening Database Connection..");
+            try
+            {
+                if (Conn.State != ConnectionState.Open)
+                {
+                    Conn.Open();
+                }
+                return true;
+            }
+            catch (OracleException oEx)
+            {
+                throw oEx;
+            }
+        }
+        /// <summary>
+        /// Closes the connection with the database.
+        /// </summary>
+        /// <returns>A bool wether the connection was successfully closed</returns>
+        public bool CloseConnection()
+        {
+            Console.WriteLine(Conn.State == System.Data.ConnectionState.Closed
+                ? "Database Connection was already closed"
+                : "Closing Database Connection..");
+            try
+            {
+                Conn.Close();
+                return true;
+            }
+            catch (OracleException oEx)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// data lezen
+        /// </summary>
+        /// <param name="sqlC"></param>
+        /// <returns></returns>
+        private OracleDataReader ReadData(OracleCommand sqlC)
+        {
+            if (!OpenConnection())
+                return null;
+            try
+            {
+                sqlC.Connection = Conn;
+                return sqlC.ExecuteReader();
+            }
+            catch (OracleException oEx)
+            {
+                CloseConnection();
+                return null;
+            }
+            catch (NullReferenceException exception)
+            {
+                return null;
+            }
+        }
     }
 }
